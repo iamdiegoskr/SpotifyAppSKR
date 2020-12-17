3c44c0828781b8337e067d73fa68dd78fec39115
diego martinez <juandiego337@hotmail.com>
2020-12-16 22:50:56 -0500

    Use spotify api with httpClient

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-// import {HttpClient} from '@angular/common/http'
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
 
-  constructor() { 
-
-
-  }
-
-  ngOnInit(): void {
+  constructor(private spotifyservice: SpotifyService) { 
+    this.spotifyservice.getNewReleases();
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,27 @@ import { Injectable } from '@angular/core';
 })
 export class SpotifyService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+    console.log("Servicio de spotify esta listo");
+  }
+  
+
+  getNewReleases(){
+
+    //aqui pongo los headers que la peticion de spotify requiere.
+    const headers = new HttpHeaders({
+      
+      'Authorization': 'Bearer BQByvIrzkLFstCyt3CyYDn9kTlErJobU8OJynxWNLXXBnCzswOjG5Pox6Ghy5nv3nUQfhh_O4oa8FFSC8S0'
+
+    })
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases',{headers})
+      .subscribe(data =>{
+        console.log(data)
+      })
+      
+  }
+
 }
+
+
